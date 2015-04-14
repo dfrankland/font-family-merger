efaf6b10e72476aba4348f1e7b5b8f5e51791be6
SnackCode <dylan.frankland@gmail.com>
2015-04-13 21:29:29 -0700

    Added comments, try fixing fonts before changing them

--- font_family_merger.py
+++ font_family_merger.py
@@ -1,28 +1,109 @@
+# For getting files:
 import os
+
+# For moving files around:
 import shutil
+
+# For converting fonts to xml and vice versa:
 from fontTools import ttx
+
+# For processing the xml from fonts
 import lxml
 from bs4 import BeautifulSoup
 from bs4 import NavigableString
 
-def parseFont(files):
+def changeFont(files):
     for file in files:
+        print "Begin processing: " + file
+
+        # Get data from xml file and close it
         with open(file, 'r+') as fileOpen:
             data = fileOpen.read()
             fileOpen.close()
+
+        # Parse xml data with BeautifulSoup
         soup = BeautifulSoup(data,'xml')
+        names = []
+
+        #Main platformIDs and nameIDs to check for
+        nameID = {
+            '1': {
+                '1': False,
+                '2': False
+            },
+            '3': {
+                '1': False,
+                '2': False
+            }
+        }
+
+        # Get the name from the namerecords
         for name in soup.findAll('namerecord'):
+            # Get the xml attributes for the namerecord tag
             nameAttrs = dict(name.attrs)
-            if nameAttrs[u'nameID'] == "1":
-                names = name.string.lstrip().split(" ",1)
-                name.string.replace_with(names[0])
-            if nameAttrs[u'nameID'] == "2":
-                name.string.replace_with(names[1])
-        newFileName = names[0] + "-" + names[1].rstrip() + ".ttx"
-        data = soup.prettify("utf-8")
-        with open(file, 'w') as fileClose:
-            fileClose.write(data)
-        os.renames(file, newFileName)
+            # Only check the main platformIDs
+            if (nameAttrs[u'platformID'] == "1" or nameAttrs[u'platformID'] == "3"):
+                # If the namerecord is there then it is found
+                nameID[nameAttrs[u'platformID']][nameAttrs[u'nameID']] = True
+                # Set the names of the file if they exist and aren't set
+                if name.string and not name.string.isspace() and not names:
+                    print "This is the name to regex: " + name.string.strip(' \t\n\r')
+                    # However you want to seperate the font family from
+                    # its variation, insert that regex here
+                    regex = ""
+                    repeat = 1
+                    names = name.string.strip(' \t\n\r').split(regex,repeat)
+                    # Sometimes the normal fonts have no variation name
+                    if len(names) == 1:
+                        names.append("Regular")
+                    elif names[1].isspace():
+                        names[1] = "Regular"
+
+        # Print the new names
+        print "This will be the font family: " + names[0]
+        print "This will be the font variation: " + names[1]
+
+        # Check if the main nameIDs are missing
+        for ID, platform in nameID.iteritems():
+            for ID, found in platform.iteritems():
+                if not found:
+                    # Add missing namerecord
+                    namerecord = soup.new_tag(
+                        "namerecord",
+                        nameID=ID,
+                        platformID="1",
+                        platEncID="1",
+                        langID="0x0"
+                    )
+                    soup.find('name').insert(int(ID), namerecord)
+
+        # Change the name in the namerecords
+        for name in soup.findAll('namerecord'):
+            # Get the xml attributes for the namerecord tag
+            nameAttrs = dict(name.attrs)
+            # Only check the main platformIDs
+            if nameAttrs[u'platformID'] == "1" or nameAttrs[u'platformID'] == "3":
+                if nameAttrs[u'nameID'] == "1":
+                    #Clear the namerecord if we can
+                    if type(name) != "NoneType":
+                        name.clear();
+                    name.append(names[0])
+                if nameAttrs[u'nameID'] == "2":
+                    #Clear the namerecord if we can
+                    if type(name) != "NoneType":
+                        name.clear();
+                    name.append(names[1])
+
+        # Write the data to a file if names were found
+        if names:
+            newFileName = names[0] + "-" + names[1] + ".ttx"
+            data = soup.prettify("utf-8")
+            with open(file, 'w') as fileClose:
+                fileClose.write(data)
+            os.renames(file, newFileName)
+        else:
+            raise Exception("No font names found!")
+        print "-----------------------------------"
 
 def getFilesWith(extension):
     files = []
@@ -32,15 +113,44 @@ def getFilesWith(extension):
     return files
 
 if __name__ == "__main__":
+    # Create a directory to do work in
     if not os.path.exists("merged_fonts"):
         os.makedirs("merged_fonts")
+
+    # Copy all fonts to our directory so we don't corrupt the originals
     for file in getFilesWith(".ttf"):
         shutil.copy(file,"merged_fonts")
+
+    # Change our working directory to our new one
     os.chdir("merged_fonts")
-    ttx.main(getFilesWith(".ttf"))
+
+    #Convert fonts to xml and catch any exceptions
+    for file in getFilesWith(".ttf"):
+        try:
+            ttx.main([file])
+            print "-----------------------------------"
+        except Exception as e:
+            print "Something went wrong converting ttf -> ttx:"
+            print e
+            pass
+
+    # Remove the old fonts that have been converted to xml
     for file in getFilesWith(".ttf"):
         os.remove(file)
-    parseFont(getFilesWith(".ttx"))
-    ttx.main(getFilesWith(".ttx"))
+
+    # Change font names
+    changeFont(getFilesWith(".ttx"))
+
+    # Convert xml back to fonts
+    for file in getFilesWith(".ttx"):
+        try:
+            ttx.main([file])
+            print "-----------------------------------"
+        except Exception as e:
+            print "Something went wrong converting ttx -> ttf:"
+            print e
+            pass
+
+    # Remove the old xml that have been converted back to fonts
     for file in getFilesWith(".ttx"):
         os.remove(file)
